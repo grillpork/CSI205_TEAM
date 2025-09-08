5eeb9833fd97ccdea5bcf8759dc62fb75e040b42
Tripop Saibut <tripopsaibut@gmail.com>
2025-09-08 20:59:37 +0700

    update comp muping

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Pooh from './components/pooh'
-import Noodee from './components/noodee'
-import Boon from './components/Boon'
-import Muping from './components/muping'
-import Bright from './components/bright'
-
+import Pooh from "./components/pooh";
+import Noodee from "./components/noodee";
+import Boon from "./components/Boon";
+import Bright from "./components/bright";
+import Muping from "./components/Muping";
 
 const App = () => {
   return (
-    <div>
-    <Pooh/>
-    <Noodee/>
-    <Boon/>
-    <Muping/>
-    <Bright/>
-    </div>
-  )
-}
+    <section className=" h-screen w-full">
+      <h1 className=" absolute top-24 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl font-black">Member's group our group</h1>
+      <div className="grid grid-cols-5 place-content-center w-full h-full p-4">
+        <Muping />
+        <Boon />
+        <Pooh />
+        <Noodee />
+        <Bright />
+      </div>
+    </section>
+  );
+};
 
-export default App
+export default App;
