590fc9fd43ff3469e0ec20bfc6918aee20541ac8
Patjinun <patjinun3800@gmail.com>
2025-09-09 20:12:30 +0700

    Update noodee

--- src/components/Noodee.jsx
+++ src/components/Noodee.jsx
@@ -5,17 +5,17 @@ const Noodee = () => {
     <div className=" p-4 rounded-2xl shadow-2xl w-fit space-y-2 font-bold">
       <img
         className=" rounded-2xl w-sm"
-        src="/src/assets/muping.jpg"
-        alt="muping"
+        src="/src/assets/noodee.jpg"
+        alt="noodee"
       />
       <div className=" space-y-2 px-4">
         <p>
-          Id : <span>67118021</span>
+          Id : <span>67118343</span>
         </p>
         <p>
-          Name : <span>Tripop Saibut</span>
+          Name : <span>Patjinun Wongkasiwat</span>
         </p>
-        <p>"The secret to getting ahead is getting started."</p>
+        <p>"It’s the little things in life."</p>
       </div>
     </div>
   )
