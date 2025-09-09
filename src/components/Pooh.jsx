c44d5dac0f1e5f65b7b5c8cd44afc0a63f7923e5
MaiDai01 <bnon5696@gmail.com>
2025-09-09 23:02:50 +0700

    Pooh
    Upd image

--- src/components/Pooh.jsx
+++ src/components/Pooh.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Pooh = () => {
   return (
-    <div>
-      
+    <div className="bg-white p-4 rounded-2xl shadow-2xl w-full font-bold">
+      <img
+        className=" rounded-2xl w-full objent-cover"
+        src="/src/assets/Pooh.jpg"
+        alt="Pooh"
+      />
+      <div className=" space-y-2 px-4 mt-4 text-[#303030]">
+        <p>
+          Id : <span>67090746</span>
+        </p>
+        <p>
+          Name : <span>Tanagorn Thitiputtaprasat</span>
+        </p>
+        <p>"Life happens, coffee helps"</p>
+      </div>
     </div>
   )
 }
